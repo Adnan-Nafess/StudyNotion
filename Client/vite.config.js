5a793b8047ff9f2c37b355efb673a9ccf59a2a0f
Mohd Adnan <mohdadnan3508@gmail.com>
2025-07-26 16:35:55 +0530

    Fix deployment config

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -3,4 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+   base: '/', // Ensure this is set correctly
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
 })
