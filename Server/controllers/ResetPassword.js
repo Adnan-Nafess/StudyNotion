7eb3cb90c68de2c0127875952e566cd796ab9b7c
Mohd Adnan <mohdadnan3508@gmail.com>
2025-07-27 10:26:35 +0530

    Reset Password update

--- Server/controllers/ResetPassword.js
+++ Server/controllers/ResetPassword.js
@@ -31,7 +31,7 @@ exports.resetPasswordToken = async (req, res) => {
 		await mailSender(
 			email,
 			"Password Reset",
-			passwordUpdated(url)
+			`Your Link for email verification is ${url}. Please click this url to reset your password.`
 		);
 
 		res.json({
